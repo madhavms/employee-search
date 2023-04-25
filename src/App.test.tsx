fb821bf9475dbf661624ba996063a31f2ff10f07
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-25 21:52:51 +0100

    Added test to show matching employees when a search query is entered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen,fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import EmployeeList from "./components/EmployeeList";
 import { Employee, department, active } from "./types/employee";
+import App from './App';
+import useEmployee from "./hooks/useEmployee";
 
 let employees: Employee[] = [
   {
@@ -103,9 +105,46 @@ describe("EmployeeList", () => {
         const employeeListItem = screen.getByText(
           `${employee.firstName} ${employee.lastName} - ${employee.tel}`
         );
-        console.log("employeeListItem=", employeeListItem);
         expect(employeeListItem).toBeInTheDocument();
       }
     });
   });
 });
+
+jest.mock('chalk', () => ({
+  green: (text: string) => text,
+  blue: (text: string) => text,
+  red: (text: string) => text,
+}));
+
+jest.mock('./hooks/useEmployee');
+
+
+
+describe('Employee Search', () => {
+  test('should show only matching employees when a search query is entered', async () => {
+    const employees = [
+      { id: 1, firstName: 'John', lastName: 'Doe', department: 'Finance', email: 'johndoe@example.com', tel: '444-444-4445' },
+      { id: 2, firstName: 'Jane', lastName: 'Doe', department: 'IT', email: 'janedoe@example.com', tel: '555-555-5555' },
+      { id: 3, firstName: 'Bob', lastName: 'Smith', department: 'Finance', email: 'bobsmith@example.com', tel: '333-333-3335' },
+    ];
+
+    const data = {
+      isLoading: false,
+      isError: false,
+      employees: employees
+    };
+
+    (useEmployee as jest.Mock).mockReturnValue(data)
+    const { getByLabelText, getByText, queryByText } = render(<App />);
+    const searchInput = await getByLabelText('Employee Search:');
+    fireEvent.change(searchInput, { target: { value: 'Doe' } });
+    const johnDoe = getByText('John Doe - johndoe@example.com');
+    const janeDoe = getByText('Jane Doe - 555-555-5555');
+    const bobSmith = queryByText('Bob Smith - bobsmith@example.com');
+    expect(johnDoe).toBeInTheDocument();
+    expect(janeDoe).toBeInTheDocument();
+    expect(bobSmith).not.toBeInTheDocument();
+  });
+});
+
