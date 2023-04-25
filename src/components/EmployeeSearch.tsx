b10685ac30e0c927ae7ec4d265b50dc4f3d99f73
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 00:54:51 +0100

    Styled Employee search component using emotion

--- src/components/EmployeeSearch.tsx
+++ src/components/EmployeeSearch.tsx
@@ -1,24 +1,55 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 interface SearchProps {
   onSearch: (search: string) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const EmployeeSearch: React.FC<SearchProps> = ({ onSearch }) => {
+const Label = styled.label`
+  margin-right: 1rem;
+  font-size: 1.2rem;
+
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+  flex-grow: 1;
+
+  @media (max-width: 768px) {
+    padding: 8px;
+    font-size: 0.9rem;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 60px;
+`;
+
+const EmployeeSearch: React.FC<SearchProps> = ({ onSearch, onKeyDown }) => {
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onSearch(event.target.value);
   };
 
   return (
-    <div>
-      <label htmlFor="searchInput">Employee Search:</label>
-      <input
+    <Container>
+      <Label htmlFor="searchInput">Employee Search:</Label>
+      <Input
         id="searchInput"
         type="text"
         placeholder="Enter a name"
         onChange={handleOnChange}
+        onKeyDown={onKeyDown}
       />
-    </div>
+    </Container>
   );
 };
 
