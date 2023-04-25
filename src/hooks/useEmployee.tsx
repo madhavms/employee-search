978d22b625149498cccff60373dd12558871395b
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-25 13:13:50 +0100

    initial loading state set to True

--- src/hooks/useEmployee.tsx
+++ src/hooks/useEmployee.tsx
@@ -4,17 +4,16 @@ import axios from "axios";
 
 const useEmployee = () => {
   const [employees, setEmployees] = useState<Employee[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const getEmployees = async () => {
-      setIsLoading(true);
       try {
         const { data } = await axios("/employees.json");
-        setIsLoading(false);
         setError(null);
         setEmployees(data);
+        setIsLoading(false);
       } catch (err) {
         setIsLoading(false)
         setError("Unable to fetch employees data.");
