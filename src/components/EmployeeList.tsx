690ce03e805e6054f54ee43a4c7d281e9643c68d
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 01:38:06 +0100

    Added responsiveness to EmployeeList component

--- src/components/EmployeeList.tsx
+++ src/components/EmployeeList.tsx
@@ -7,13 +7,6 @@ interface ListProps {
   employees: Employee[];
 }
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;  
-  justify-content: center;
-  align-items: center;
-`;
-
 const List = styled.ol`
   list-style-type: none;
   padding: 0;
@@ -30,29 +23,25 @@ const ListItem = styled.li`
   box-sizing: border-box;
 
   @media (max-width: 768px) {
-    font-size: 1rem;
+    font-size: 1.1rem;
   }
 `;
 
 const EmployeeList: React.FC<ListProps> = ({ employees }) => {
   if (!employees.length) {
-    return (
-      <Container>
-        <NoEmployeesFound />
-      </Container>
-    );
+    return <NoEmployeesFound />;
   }
 
   return (
-    <Container>
-      <List>
-        {employees.map((employee) => {
-          return (
-            <ListItem key={employee.id}>{getEmployeeResultByDept(employee)}</ListItem>
-          );
-        })}
-      </List>
-    </Container>
+    <List>
+      {employees.map((employee) => {
+        return (
+          <ListItem key={employee.id}>
+            {getEmployeeResultByDept(employee)}
+          </ListItem>
+        );
+      })}
+    </List>
   );
 };
 
