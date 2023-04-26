a25329e345dc4a6daad28129563c3a012cb77a94
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 01:18:42 +0100

    Styled submit button

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,13 +1,39 @@
+import React from "react";
+import styled from "@emotion/styled";
+
 interface ButtonProps {
   handleClick: () => void;
 }
 
+const StyledButton = styled.button`
+  background-color: #0091EA;
+  color: #FFFFFF;
+  padding: 10px 20px;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  width: 250px;
+  font-size: 1.2rem;
+
+  &:hover {
+    background-color: #1976D2;
+  }
+
+  @media (max-width: 768px) {
+    padding: 8px 16px;
+    font-size: 1rem;
+    width: 220px;
+  }
+`;
+
 const SubmitButton: React.FC<ButtonProps> = ({ handleClick }) => {
   return (
-    <div>
-      <button onClick={handleClick}>Submit</button>
-    </div>
+    <StyledButton onClick={handleClick}>Submit</StyledButton>
   );
 };
 
 export default SubmitButton;
+
+
+
