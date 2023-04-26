91fbaabf1fcd45b4b2bad8cc460c3833624f42d8
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 08:09:28 +0100

    Tests for EmployeeSearch Component

---
+++ src/__tests__/components/EmployeeSearch.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import EmployeeSearch from "../../components/EmployeeSearch";
+
+describe("EmployeeSearch Component", () => {
+   test("on value change onChange is called", () => {
+         const onChange = jest.fn();
+         render(<EmployeeSearch onSearch={onChange} />);
+         const input = screen.getByRole("textbox");
+         expect(input).toBeInTheDocument();
+         expect(input).toHaveValue("");
+         fireEvent.change(input, { target: { value: "Dan" } });
+         expect(onChange).toHaveBeenCalledTimes(1);
+         expect(input).toHaveValue("Dan");
+    });
+});
