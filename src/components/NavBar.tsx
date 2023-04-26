46b68cb983240b6a3665636eb107881ae9e8d58f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 01:18:26 +0100

    Navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const NavbarContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  background-color: #0091EA;
+  color: #ffffff;
+  padding: 0 16px;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  margin: 0;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`;
+
+const Navbar: React.FC = () => {
+  return (
+    <NavbarContainer>
+      <Title>Employee Search</Title>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
