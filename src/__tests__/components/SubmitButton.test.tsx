38e01206d4b064ebabd535a56619a351f0ac416d
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 08:09:39 +0100

    Tests for SubmitButton Component

---
+++ src/__tests__/components/SubmitButton.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import SubmitButton from "../../components/SubmitButton";
+
+describe("SubmitButton", () => {
+    test("on click onClick is called", () => {
+        const onClick = jest.fn();
+        render(<SubmitButton handleClick={onClick} />);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+        fireEvent.click(button);
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+});
