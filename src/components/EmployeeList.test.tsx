057b76a51cacfdcd3886300ce40500101004d4a4
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 21:20:57 +0100

    Test restructuring

--- src/components/EmployeeList.test.tsx
+++ src/components/EmployeeList.test.tsx
@@ -1,14 +1,49 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import EmployeeList from "./EmployeeList";
+import { department } from "../types/employee";
 import { employees } from "../mock-data/employeeData";
 
-describe("EmployeeList Component", () => {
-    test("renders correct number of employees", () => {
-        render(<EmployeeList employees={employees} />);
-        const employeeList = screen.getByRole("list");
-        expect(employeeList).toBeInTheDocument();
-        const listItemElements = screen.getAllByRole('listitem');
-        expect(listItemElements).toHaveLength(employees.length);
+describe("List Functionality", () => {
+    test("renders correct number of employees", async () => {
+      render(<EmployeeList employees={employees} />);
+      const employeeList = await screen.findAllByRole("listitem");
+      employeeList.forEach((employeeListItem) => {
+        expect(employeeListItem).toBeInTheDocument();
+      });
+      expect(employeeList).toHaveLength(employees.length);
     });
-});
+  
+    test("displays correct information for employees in Finance department", () => {
+  
+      render(<EmployeeList employees={employees} />);
+      
+      const financeEmployees = employees
+        .filter((employee) => employee.department === department.FINANCE)
+        .map(
+          (employee) =>
+            `${employee.firstName} ${employee.lastName} - ${employee.email}`
+        );
+  
+        financeEmployees.forEach((financeEmployee) => {
+          const employeeListItem = screen.getByText(financeEmployee);
+          expect(employeeListItem).toBeInTheDocument();
+        });
+    });
+  
+    test("displays correct information for employees in IT department", () => {
+  
+      render(<EmployeeList employees={employees} />);
+      const itEmployees = employees
+        .filter((employee) => employee.department === department.IT)
+        .map(
+          (employee) =>
+            `${employee.firstName} ${employee.lastName} - ${employee.tel}`
+        );
+  
+      itEmployees.forEach((itEmployee) => {
+        const employeeListItem = screen.getByText(itEmployee);
+        expect(employeeListItem).toBeInTheDocument();
+      });
+    });
+  });
--- src/components/SubmitButton.test.tsx
+++ src/components/SubmitButton.test.tsx
@@ -1,14 +1,40 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, renderHook } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import SubmitButton from "../components/SubmitButton";
+import useHandleLogToConsole from "../hooks/useHandleLogToConsole";
+import { employees } from "../mock-data/employeeData";
+
+jest.mock("chalk", () => ({
+  green: (text: string) => text,
+  blue: (text: string) => text,
+  red: (text: string) => text,
+}));
 
 describe("SubmitButton", () => {
-    test("on click onClick is called", () => {
-        const onClick = jest.fn();
-        render(<SubmitButton handleClick={onClick} />);
-        const button = screen.getByRole("button");
-        expect(button).toBeInTheDocument();
-        fireEvent.click(button);
-        expect(onClick).toHaveBeenCalledTimes(1);
-    });
+  test("on click onClick is called", () => {
+    const onClick = jest.fn();
+    render(<SubmitButton handleClick={onClick} />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test("clicking the Submit button logs visible employees to the console", () => {
+    const logSpy = jest.spyOn(console, "log").mockImplementation();
+
+    // Render the SubmitButton component with the useHandleLogToConsole hook
+    const { result } = renderHook(() => useHandleLogToConsole(employees));
+    render(<SubmitButton handleClick={result.current.handleLogToConsole} />);
+    const button = screen.getByText("Submit");
+    fireEvent.click(button);
+
+    expect(logSpy).toHaveBeenCalledTimes(5); // 1 for log header and 2 for employee logs
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("Dan Mac"));
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("Jake Ross"));
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("Ian Moss"));
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("Derek Joss"));
+
+    logSpy.mockRestore(); // restore console.log to original implementation
+  });
 });
