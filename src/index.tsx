5a8304d257e7b08913fb63027f199ab026a1addf
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 09:32:35 +0100

    Added routing for gh-pages and local

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// rr - react strict mode ?
+const isGitHubPages = window.location.hostname.includes('github.io');
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={isGitHubPages ? '/employee-search' : '/'}>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
