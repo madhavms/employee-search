3054ed464e1702a74293bf33f5c964cefa602071
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 23:45:15 +0100

    Formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
