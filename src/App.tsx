ddee23f69a853e66374b279519bf0f129d61bbb3
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 01:18:05 +0100

    Style changes and log submit on input box key down

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
 import EmployeeList from "./components/EmployeeList";
-import EmployeeSearch from "./components/EmployeeSeach";
+import EmployeeSearch from "./components/EmployeeSearch";
 import SubmitButton from "./components/SubmitButton";
 import useEmployee from "./hooks/useEmployee";
 import useFilteredEmployees from "./hooks/useFilteredEmployees";
 import useHandleLogToConsole from "./hooks/useHandleLogToConsole";
+import styled from "@emotion/styled";
 import "./App.css";
+import Navbar from "./components/NavBar";
+import LoaderComponent from "./components/Loader";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100vh;
+  padding-top: 5rem;
+  gap:1rem;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 1rem;
+
+  @media (max-width: 570px) {
+    flex-direction: column;
+  }
+`;
 
 function App() {
   const { employees, isLoading, error } = useEmployee();
@@ -18,25 +44,29 @@ function App() {
     setFilterText(searchText);
   }, []);
 
+  const onKeyDown = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.currentTarget.blur(); // remove focus from input field
+      handleLogToConsole();
+    }
+  }, [handleLogToConsole]);
+
   // rr - separate component for loading and error
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <LoaderComponent/>;
   else if (error) return <div>{error}</div>;
 
   return (
     // rr - styles in .css file
-    <div
-      className="centered-container"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <EmployeeSearch onSearch={onSearch} />
-      <SubmitButton handleClick={handleLogToConsole} />
-      <EmployeeList employees={filterText ? filteredEmployees : employees} />
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Container>
+        <SearchContainer>
+          <EmployeeSearch onSearch={onSearch} onKeyDown={onKeyDown}/>
+          <SubmitButton handleClick={handleLogToConsole} />
+        </SearchContainer>
+        <EmployeeList employees={filterText ? filteredEmployees : employees} />
+      </Container>
+    </React.Fragment>
   );
 }
 
