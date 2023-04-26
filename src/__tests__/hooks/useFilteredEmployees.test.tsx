6627ab86c57c4c3111430644bb0a98e0ccdc9d9e
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 17:10:54 +0100

    Tests for useFilteredEmployee hook

---
+++ src/__tests__/hooks/useFilteredEmployees.test.tsx
@@ -0,0 +1,31 @@
+import { renderHook } from "@testing-library/react";
+import useFilteredEmployees from "../../hooks/useFilteredEmployees";
+import { employees } from "../../testData/employeeData";
+
+describe ('useFilteredEmployees hook', () => {
+    test('should return all employees when filterText is empty', () => {
+        const filterText = '';
+        const {result} = renderHook(() => useFilteredEmployees(employees, filterText));
+        expect(result.current.filteredEmployees).toEqual(employees);
+    });
+
+    test('should filter employees by first name', () => {
+        const filterText = 'Dan';
+        const {result} = renderHook(() => useFilteredEmployees(employees, filterText));
+        expect(result.current.filteredEmployees).toEqual([employees[0]]);
+    });
+
+    test('should filter employees by last name', () => {
+        const filterText = 'Moss';
+        const {result} = renderHook(() => useFilteredEmployees(employees, filterText));
+        expect(result.current.filteredEmployees).toEqual([employees[2]]);
+    });
+
+    it("should filter employees by first and last name", () => {
+        const filterText = "Derek Joss";
+        const { result } = renderHook(() =>
+          useFilteredEmployees(employees, filterText)
+        );
+        expect(result.current.filteredEmployees).toEqual([employees[3]]);
+      });
+});
