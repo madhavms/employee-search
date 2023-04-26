a7b69490700e2abbc8a0e9b8a1adf7d869ac1d82
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-26 01:38:23 +0100

    Style changes

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -10,9 +10,10 @@ export const ErrorBanner = styled.div`
   font-size: 1.2rem;
   border-radius: 4px;
   box-shadow: 0 2px 4px gray;
-  max-width: 500px;
+  width: 40vw;
 
   @media (max-width: 570px) {
-    font-size:1rem;
+    font-size:1.1rem;
+    width: 60vw;
   }
 `;
