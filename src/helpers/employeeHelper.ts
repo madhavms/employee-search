8da548259ec955ee43d3df223b55d229aa9c979a
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-05-10 19:04:20 +0100

    Moved getEmployees logic to helper and also added method to abort network

--- src/helpers/employeeHelper.ts
+++ src/helpers/employeeHelper.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Employee, department } from "../types";
 
 const getName = (employee: Employee): string => {
@@ -6,26 +7,51 @@ const getName = (employee: Employee): string => {
 
 const departmentRules = {
    [department.FINANCE]: {
-      callback: (employee: Employee) => {
+      rule: (employee: Employee) => {
          return `${getName(employee)} - ${employee.email}`;
       },
    },
 
    [department.IT]: {
-      callback: (employee: Employee) => {
+      rule: (employee: Employee) => {
          return `${getName(employee)} - ${employee.tel}`;
       },
    },
 };
 
 export const getEmployeeResultByDept = (employee: Employee): string => {
-   return departmentRules[employee.department].callback(employee);
+   return departmentRules[employee.department].rule(employee);
 };
 
 export const filterEmployeeList = (employee: Employee, filterText: string) => {
-   const {firstName, lastName, email, tel} = employee;
+   const { firstName, lastName, email, tel } = employee;
    const searchText = filterText.toLowerCase();
-   return `${firstName} ${lastName}`.toLowerCase().includes(searchText)
-       || email.toLowerCase().includes(searchText)
-       || `${tel}`.includes(searchText);
+   return (
+      `${firstName} ${lastName}`.toLowerCase().includes(searchText) ||
+      email.toLowerCase().includes(searchText) ||
+      `${tel}`.includes(searchText)
+   );
+};
+
+let abortController: AbortController;
+let pathToEmployeeData: string = process.env.REACT_APP_EMPLOYEE_DATA_PATH
+   ? `${process.env.REACT_APP_EMPLOYEE_DATA_PATH}`
+   : "/employees.json";
+
+export const getEmployees = async () => {
+   abortController = new AbortController();
+   const signal = abortController.signal;
+
+   try {
+      const { data } = await axios(pathToEmployeeData, {signal});
+      return data;
+   }
+   catch (error) {
+      throw new Error("Unable to fetch employees data.");
+   }
+};
+
+export const getEmployeesCleanUp = () => {
+   if(abortController.signal)
+      abortController.abort();
 };
